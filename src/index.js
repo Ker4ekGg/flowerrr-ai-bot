bf92df8226d4695cf5f87ea181d81d0aa14419cd
Ker4ekGg <karen.akopyan2004@mail.ru>
2026-09-02 20:10:15 +0300

    теперь точно

--- src/index.js
+++ src/index.js
@@ -1432,12 +1432,11 @@ await sendAdminMessage(
 );
 
 return new Response("OK");
+     } catch (error) {
 
-      } catch (error) {
+      console.error("TELEGRAM WEBHOOK ERROR:", error);
 
-        console.error("TELEGRAM WEBHOOK ERROR:", error);
-
-        return new Response("OK");
+      return new Response("OK");
       }
 
       return new Response("FLOWERRR AI 🌸");
